17304c78dcfc29f26e011455bc0b43b3f2898559
GeoRegeAlin <georegealin@gmail.com>
2019-07-03 02:00:25 +0300

    Add files via upload

--- Prediction.py
+++ Prediction.py
@@ -3,26 +3,25 @@ from keras.preprocessing import image
 from skimage import color, transform
 import numpy as np
 import json
-IMG_SIZE = 48
 
 def preprocess_img(img):
     hsv = color.rgb2hsv(img)
     img = color.hsv2rgb(hsv)
-    img = transform.resize(img, (IMG_SIZE, IMG_SIZE))
+    img = transform.resize(img, (48, 48))
 
     img = np.rollaxis(img, -1)
 
     return img
 
-def clasificare(id):
+def classification(id):
     with open('IdentitateSemn.txt') as json_file:
         data=json.load(json_file)
     print(data[str(id)])
 
 
 model = load_model("CNNmodel.h5")
-new_image = image.load_img("Examples/5.jfif")
+new_image = image.load_img("Examples/Limita20km.jfif")
 new_image=preprocess_img(new_image)
 image = np.expand_dims(new_image, axis=0)
 pred = model.predict(image)
-print(clasificare(str(np.argmax(pred)+1)))
+print(classification(str(np.argmax(pred)+1)))
--- TrainModel.py
+++ TrainModel.py
@@ -1,17 +1,16 @@
 import numpy as np
-from keras.utils import plot_model
-from skimage import color, exposure, transform,io
+from skimage import color, transform
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Flatten
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras import optimizers
-from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from keras.callbacks import ModelCheckpoint
 from keras import backend as K
 K.set_image_data_format('channels_first')
 import os
 import glob
-
+import cv2
 
 def preprocess_img(img):
     hsv = color.rgb2hsv(img)
@@ -26,17 +25,17 @@ def get_class(img_path):
 img_dir = 'GTSRB/Training/images/'
 imgs = []
 labels = []
-
 all_img_paths = glob.glob(os.path.join(img_dir, '*/*.ppm'))
 np.random.shuffle(all_img_paths)
 for img_path in all_img_paths:
-    img = preprocess_img(io.imread(img_path))
+    img = preprocess_img(cv2.imread(img_path))
     label = get_class(img_path)
     imgs.append(img)
     labels.append(label)
 
 X = np.array(imgs, dtype='float32')
 Y = np.eye(43, dtype='uint8')[labels]
+
 
 def cnn_model():
     model = Sequential([Conv2D(16, (3, 3), padding='same',
@@ -68,17 +67,13 @@ model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
 
-def LRS(epoch):
-    return 0.01 * (0.01 ** int(epoch / 10))
-
-batch_size = 50
-epochs = 10
+batch_size = 100
+epochs = 20
 split_data=0.2
 
 model.fit(X, Y,
           batch_size=batch_size,
           epochs=epochs,
           validation_split=split_data,
-          callbacks=[LearningRateScheduler(LRS),
-                     ModelCheckpoint('CNNmodel.h5', save_best_only=True)]
+          callbacks=[ModelCheckpoint('CNNmodel.h5', save_best_only=True)]
           )
